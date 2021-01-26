f52ceb49eac335bb5becc8048a53139b87013317
Jesuloba <jesulobajohn@gmail.com>
2021-01-26 15:12:24 +0100

    Added vertical and horizontal Labels

--- src/components/BoardCells/BoardCell/BoardCell.js
+++ src/components/BoardCells/BoardCell/BoardCell.js
@@ -1,9 +1,25 @@
 import classes from "./BoardCell.module.css";
 
 const BoardCell = (props) => {
-	const cellClasses = [classes[props.color], classes[props.position]];
+	const cellClasses = [
+		classes[props.color],
+		classes[props.position],
+		classes.cell,
+	];
 
-	return <div className={cellClasses.join(" ")}></div>;
+	const Labelvertical = [classes.Label, classes.LabelVertical];
+	const Labelhorizontal = [classes.Label, classes.LabelHorizontal];
+
+	return (
+		<div className={cellClasses.join(" ")}>
+			{props.vertical ? (
+				<p className={Labelvertical.join(" ")}>{props.vertical}</p>
+			) : null}
+			{props.horizontal ? (
+				<p className={Labelhorizontal.join(" ")}>{props.horizontal}</p>
+			) : null}
+		</div>
+	);
 };
 
 export default BoardCell;
--- src/components/BoardCells/BoardCells.js
+++ src/components/BoardCells/BoardCells.js
@@ -3,7 +3,7 @@ import BoardCell from "./BoardCell/BoardCell";
 
 const Boardcells = (props) => {
 	const cells = [
-		{ color: "white", position: "H1" },
+		{ color: "white", position: "H1", labelVertical: "H" },
 		{ color: "black", position: "H2" },
 		{ color: "white", position: "H3" },
 		{ color: "black", position: "H4" },
@@ -18,8 +18,8 @@ const Boardcells = (props) => {
 		{ color: "white", position: "G4" },
 		{ color: "black", position: "G3" },
 		{ color: "white", position: "G2" },
-		{ color: "black", position: "G1" },
-		{ color: "white", position: "F1" },
+		{ color: "black", position: "G1", labelVertical: "G" },
+		{ color: "white", position: "F1", labelVertical: "F" },
 		{ color: "black", position: "F2" },
 		{ color: "white", position: "F3" },
 		{ color: "black", position: "F4" },
@@ -34,8 +34,8 @@ const Boardcells = (props) => {
 		{ color: "white", position: "E4" },
 		{ color: "black", position: "E3" },
 		{ color: "white", position: "E2" },
-		{ color: "black", position: "E1" },
-		{ color: "white", position: "D1" },
+		{ color: "black", position: "E1", labelVertical: "E" },
+		{ color: "white", position: "D1", labelVertical: "D" },
 		{ color: "black", position: "D2" },
 		{ color: "white", position: "D3" },
 		{ color: "black", position: "D4" },
@@ -50,8 +50,8 @@ const Boardcells = (props) => {
 		{ color: "white", position: "C4" },
 		{ color: "black", position: "C3" },
 		{ color: "white", position: "C2" },
-		{ color: "black", position: "C1" },
-		{ color: "white", position: "B1" },
+		{ color: "black", position: "C1", labelVertical: "C" },
+		{ color: "white", position: "B1", labelVertical: "B" },
 		{ color: "black", position: "B2" },
 		{ color: "white", position: "B3" },
 		{ color: "black", position: "B4" },
@@ -59,20 +59,30 @@ const Boardcells = (props) => {
 		{ color: "black", position: "B6" },
 		{ color: "white", position: "B7" },
 		{ color: "black", position: "B8" },
-		{ color: "white", position: "A8" },
-		{ color: "black", position: "A7" },
-		{ color: "white", position: "A6" },
-		{ color: "black", position: "A5" },
-		{ color: "white", position: "A4" },
-		{ color: "black", position: "A3" },
-		{ color: "white", position: "A2" },
-		{ color: "black", position: "A1" },
+		{ color: "white", position: "A8", labelHorizontal: "8" },
+		{ color: "black", position: "A7", labelHorizontal: "7" },
+		{ color: "white", position: "A6", labelHorizontal: "6" },
+		{ color: "black", position: "A5", labelHorizontal: "5" },
+		{ color: "white", position: "A4", labelHorizontal: "4" },
+		{ color: "black", position: "A3", labelHorizontal: "3" },
+		{ color: "white", position: "A2", labelHorizontal: "2" },
+		{
+			color: "black",
+			position: "A1",
+			labelVertical: "A",
+			labelHorizontal: "1",
+		},
 	];
 
 	return (
 		<Fragment>
 			{cells.map((cell) => (
-				<BoardCell color={cell.color} position={cell.position} />
+				<BoardCell
+					color={cell.color}
+					position={cell.position}
+					vertical={cell.labelVertical}
+					horizontal={cell.labelHorizontal}
+				/>
 			))}
 		</Fragment>
 	);
