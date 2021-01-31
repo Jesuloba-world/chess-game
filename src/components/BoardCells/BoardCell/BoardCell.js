2785d95eb9af4d159d4b0c61c18736dc967ef2fb
Jesuloba <jesulobajohn@gmail.com>
2021-01-31 18:10:29 +0100

    boardcells can toggle active state

--- src/components/BoardCells/BoardCell/BoardCell.js
+++ src/components/BoardCells/BoardCell/BoardCell.js
@@ -1,4 +1,6 @@
 import classes from "./BoardCell.module.css";
+import { useDispatch } from "react-redux";
+import { clearActive } from "../../../store/action";
 
 const BoardCell = (props) => {
 	const cellClasses = [
@@ -10,8 +12,14 @@ const BoardCell = (props) => {
 	const Labelvertical = [classes.Label, classes.LabelVertical];
 	const Labelhorizontal = [classes.Label, classes.LabelHorizontal];
 
+	const dispatch = useDispatch();
+
+	const onClickHandler = () => {
+		dispatch(clearActive());
+	};
+
 	return (
-		<div className={cellClasses.join(" ")}>
+		<div className={cellClasses.join(" ")} onClick={onClickHandler}>
 			{props.vertical ? (
 				<p className={Labelvertical.join(" ")}>{props.vertical}</p>
 			) : null}
